cdce975ef86eccfefbc31bde9c3a5ef901c8419f
Mohammad Yousaf <yousafmasoodi24@gmail.com>
2026-04-21 02:28:49 +0430

    feature: build home search bar ui

--- client/airbnb/src/pages/HomePage.jsx
+++ client/airbnb/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HomeNavbar from "../components/home/HomeNavbar";
+import HomeSearchBar from "../components/home/HomeSearchBar";
 
 export default function HomePage() {
   return (
     <div className="home-page">
       <HomeNavbar />
+      <HomeSearchBar/>
     </div>
   );
 }
