7014c85b9328d34cbec6196365e0584692071c71
Mohammad Yousaf <yousafmasoodi24@gmail.com>
2026-04-21 03:24:01 +0430

    feature: build home listings grid ui

--- client/airbnb/src/pages/HomePage.jsx
+++ client/airbnb/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import HomeNavbar from "../components/home/HomeNavbar";
 import HomeSearchBar from "../components/home/HomeSearchBar";
 
 import HomeCategories from "../components/home/HomeCategories"
+import HomeListingsGrid from "../components/home/HomeListingsGrid";
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <HomeNavbar />
       <HomeSearchBar/>
       <HomeCategories/>
+      <HomeListingsGrid/>
     </div>
   );
 }
