06cdd25930cbedc937d3be7b251e6a865c45fd9d
Mohammad Yousaf <yousafmasoodi24@gmail.com>
2026-04-22 06:16:16 +0430

    feature: build property summary section ui

--- client/airbnb/src/pages/PropertyDetailsPage.jsx
+++ client/airbnb/src/pages/PropertyDetailsPage.jsx
@@ -2,13 +2,14 @@ import PropertyNavbar from "../components/property/PropertyNavbar";
 
 import PropertyGallery from "../components/property/PropertyGallery";
 
-
+import PropertySummary from "../components/property/PropertySummary";
 
 export default function PropertyDetailsPage() {
   return (
     <div>
       <PropertyNavbar />
       <PropertyGallery />
+      <PropertySummary/>
     </div>
   );
 }
